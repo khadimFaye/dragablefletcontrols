2e75bcb2a1c44b77697563f6927eae6584d37413
Cheikhul Xadim Faye <97457513+khadimFaye@users.noreply.github.com>
2024-09-20 14:51:02 +0000

    firt commit

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from flet import Column, Page, GestureDetector, Image, Colors, Row, Text, MainAxisAlignment, DragUpdateEvent, App
+from flet import Column, Page, GestureDetector, Image, Row, Text, MainAxisAlignment, DragUpdateEvent, app, Container
 
 
-def main(psge:Page):
+def main(page:Page):
   
-  def makeMagic(DragUpdateEvent, control):
-    if control.spacing1!=-5:
+  def makeMagic(e:DragUpdateEvent, control):
+    print('chiamato')
+    print(e.delta_x)
+    if control.spacing!=-5 and e.delta_x <0.7:
       control.spacing=-5
-    else:
+    elif control.spacing==-5 and e.delta_x >-0.7:
       control.spacing=5
     control.update()
     page.update()
+  
+  def test(e:DragUpdateEvent):
+    print(e.delta_x)
     
 
-  row_image = Row(alignment = MainAxisAlignment.CENTER, spacing = 1)
-  gsdetector = GestureDetector(content = row_image, on_pan_update = lambda DragUpdateEvent :makeMagic(DragUpdateEvent, row_image)
+  row_image = Row(alignment = MainAxisAlignment.CENTER, spacing = 1, controls = [Container(width = 50, height = 50, bgcolor = 'blue'), Container(width = 50, height = 50, bgcolor = 'red')])
+  gsdetector = GestureDetector(content = row_image, on_pan_update = lambda DragUpdateEvent :makeMagic(DragUpdateEvent, row_image), multi_tap_touches = lambda DragUpdateEvent : test(DragUpdateEvent))
   
   layout1 = Column(controls=[
-    Row(alignment= MainAxisAlignment.CENTER, controls=[ Text(value = 'pizzica per vedere la magia ;)',weight = 'w700',size = 20, expand = True),
+    Row(alignment= MainAxisAlignment.CENTER, controls=[ Text(value = 'pizzica per vedere la magia ;)',weight = 'w700',size = 20, expand = False)]),
     gsdetector                                              
                                                        
   ]
@@ -24,5 +29,5 @@ def main(psge:Page):
   page.add(layout1)
   page.update()
 
-App(target = main)
+app(target = main)
 
